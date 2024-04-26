f66de7e6360a7c4a99df5500e12652455c166528
Anglad <ya.anton-glladkov02@yandex.ru>
2024-04-26 18:04:13 +0300

    Add project files.

---
+++ app/views.py
@@ -0,0 +1,119 @@
+﻿from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from .forms import AnketaForm  
+"""
+Definition of views.
+"""
+
+from datetime import datetime
+from django.shortcuts import render
+from django.http import HttpRequest
+
+from app.forms import AnketaForm
+
+def home(request):
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/index.html',
+        {
+            'title':'Главная',
+            'year':datetime.now().year,
+        }
+    )
+
+def contact(request):
+    """Renders the contact page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/contact.html',
+        {
+            'title':'Контакты',
+            'message':'Вы можете найти нас по адресу:   ',
+            'year':datetime.now().year,
+        }
+    )
+
+def about(request):
+    """Renders the about page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/about.html',
+        {
+            'title':'О нас',
+            'message':'Сведения о нас.',
+            'year':datetime.now().year,
+        }
+    )
+
+def links(request):
+    assert isinstance(request, HttpRequest)
+    return render(
+        request, 
+        'app/links.html',
+        {
+            'title':'Ссылки',
+            'message':'Ссылки на схожие ресурсы:',
+            'year':datetime.now().year,
+        }
+    )
+
+def anketa(request):
+    assert isinstance(request, HttpRequest)
+    data = None
+    gender = {'1': 'Мужчина', '2': 'Женщина'}
+    internet = {'1': 'Каждый день', '2': 'Несколько раз в день',
+                '3': "Несколько раз в неделю", '4': 'Несколько раз в месяц'}
+    if request.method == 'POST':
+        form = AnketaForm(request.POST)
+        if form.is_valid():
+            data = dict()
+            data['name'] = form.cleaned_data['name']
+            data['city'] = form.cleaned_data['city']
+            data['job'] = form.cleaned_data['job']
+            data['gender'] = gender[ form.cleaned_data['gender'] ]
+            data['internet'] = internet[ form.cleaned_data['internet']]
+            if(form.cleaned_data['notice'] == True):
+                data['notice'] = "Да"
+            else:
+                data['notice'] = "Нет"
+                data['email'] = form.cleaned_data['email']
+                data['message'] = form.cleaned_data['message']
+                form = None
+    else:
+        form = AnketaForm()
+    return render(
+        request,
+        'app/anketa.html',
+        {
+            'form':form,
+            'data':data
+        }
+    )
+
+def registration(request):
+    assert isinstance(request, HttpRequest)
+    if request.method == "POST": # после отправки формы
+        regform = UserCreationForm(request.POST)    
+        if regform.is_valid(): #валидация полей формы
+            reg_f = regform.save(commit=False) # не сохраняем автоматически данные формы
+            reg_f.is_staff = False # запрещен вход в административный раздел
+            reg_f.is_active = True # активный пользователь
+            reg_f.is_superuser = False # не является суперпользователем
+            reg_f.date_joined = datetime.now() # дата регистрации
+            reg_f.last_login = datetime.now() # дата последней авторизации
+            reg_f.save() # сохраняем изменения после добавления данных
+        return redirect('home') # переадресация на главную страницу после регистрации
+    else:
+        regform = UserCreationForm() # создание объекта формы для ввода данных нового пользователя
+    return render(
+        request,
+        'app/registration.html',
+        {
+            'regform': regform, # передача формы в шаблон веб-страницы
+            'year':datetime.now().year,
+        }
+    )
